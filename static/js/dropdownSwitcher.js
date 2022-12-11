849d53f8e025333d542c2cfd1cb2fb028c025c86
Irina-M-lu <freelancelotta@gmail.com>
2022-12-11 18:46:12 +0100

    dropdownSwitcher + code fixed

--- static/js/dropdownSwitcher.js
+++ static/js/dropdownSwitcher.js
@@ -5,17 +5,15 @@ dropdownSwitcherList.forEach((element) => {
     evt.preventDefault();
     const contentDropdown = element.parentElement.querySelector('.content-dropdown');
     if (contentDropdown.classList.contains('hidden')) {
-  dropdownSwitcherList.forEach((element) => {
-    element.parentElement.querySelector('.content-dropdown').classList.add('hidden');
-  }
-      )
-   
+      dropdownSwitcherList.forEach((element) => {
+        element.parentElement.querySelector('.content-dropdown').classList.add('hidden');
+        });
       contentDropdown.classList.remove('hidden');
       contentDropdown.classList.add('flex');
     } else {
       contentDropdown.classList.add('hidden');
       contentDropdown.classList.remove('flex');
     }
- 
   });
 });
+
