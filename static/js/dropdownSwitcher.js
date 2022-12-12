5a5e9881f751db633bf585dc95b40611966e8309
Irina-M-lu <freelancelotta@gmail.com>
2022-12-12 20:34:17 +0100

    new dropMenu version

--- static/js/dropdownSwitcher.js
+++ static/js/dropdownSwitcher.js
@@ -4,16 +4,16 @@ dropdownSwitcherList.forEach((element) => {
   element.addEventListener('click', (evt) => {
     evt.preventDefault();
     const contentDropdown = element.parentElement.querySelector('.content-dropdown');
+  
     if (contentDropdown.classList.contains('hidden')) {
-      dropdownSwitcherList.forEach((element) => {
-        element.parentElement.querySelector('.content-dropdown').classList.add('hidden');
-        });
+      document.querySelectorAll('.content-dropdown').forEach((item) => item.classList.add('hidden'));
       contentDropdown.classList.remove('hidden');
       contentDropdown.classList.add('flex');
     } else {
       contentDropdown.classList.add('hidden');
       contentDropdown.classList.remove('flex');
     }
+   });
   });
-});
+
 
