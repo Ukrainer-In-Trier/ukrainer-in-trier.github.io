74587acbd80622d76e426e174c1028f584bed5e8
Likita <Likita@users.noreply.github.com>
2022-12-13 19:13:16 +0000

    deploy: 9339841a853b99fac53bb5a5f679c89a8a4e4213

--- js/dropdownSwitcher.js
+++ js/dropdownSwitcher.js
@@ -4,12 +4,16 @@ dropdownSwitcherList.forEach((element) => {
   element.addEventListener('click', (evt) => {
     evt.preventDefault();
     const contentDropdown = element.parentElement.querySelector('.content-dropdown');
+  
     if (contentDropdown.classList.contains('hidden')) {
+      document.querySelectorAll('.content-dropdown').forEach((item) => item.classList.add('hidden'));
       contentDropdown.classList.remove('hidden');
       contentDropdown.classList.add('flex');
     } else {
       contentDropdown.classList.add('hidden');
       contentDropdown.classList.remove('flex');
     }
+   });
   });
-});
+
+
